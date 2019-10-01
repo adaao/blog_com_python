3c930023db81f42b234631d52a5548aae2d287b6
= <=>
2019-10-01 16:34:42 -0300

    criação do projeto e configuração do server

---
+++ microblog/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\adaao\documents\dev\blog_em_python\blog_com_python\microblog\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
