db72046eeafa5987d08767034913089a8494c304
= <=>
2019-10-01 18:13:45 -0300

    Olá mundo, Django

---
+++ microblog/django_app/urls.py
@@ -0,0 +1,6 @@
+from django.conf.urls import patterns, url
+from django_app import views
+
+urlpatterns = patterns('',
+                       url(r'^', views.index, name='index'))
+
--- microblog/django_project/urls.py
+++ microblog/django_project/urls.py
@@ -18,4 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^django_app/', include('django_app.urls')),
 ]
